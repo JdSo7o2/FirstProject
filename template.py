3f1cb04cd6c024d6a7118527681a4d80b21c9efa
Chaitanya <cdbwork07@gmail.com>
2024-05-20 15:02:43 +0530

    Modified Tests Folder

--- template.py
+++ template.py
@@ -15,7 +15,7 @@ list_of_files = [
     "src/utils/utils.py",
     "src/logger/logging.py",
     "src/exception/exception.py",
-    "tests/utils/unit/__init__.py",
+    "tests/unit/__init__.py",
     "tests/integration/__init__.py",
     "init_setup.sh",
     "requirements.txt",
